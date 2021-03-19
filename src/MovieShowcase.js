107ede42d46dcdb9b96966fa8a6cb3e51f49fc2d
reevesalic <reevesalic@gmail.com>
2021-03-19 15:05:46 -0400

    commit tree 3534d1d51927058099111ee91d5e270aecc38858

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,12 +6,12 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
-  }
+  return movieData.map((data, idx) => <MovieCard key={idx} {...data} />);
+  };
 
   render() {
     return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
+      <div id="movie-showcase">{this.generateMovieCards()}
       </div>
     )
   }
